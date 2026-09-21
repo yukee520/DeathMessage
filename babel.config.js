9c8d7a40535048b724fbd9907dc9773ec1213e6e
yukee520 <yukee92tmc@hotmail.com>
2026-09-22 00:21:57 +0800

    Update babel.config.js [skip ci]

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: ['react-native-reanimated/plugin'],
+  presets: [
+    'module:@react-native/babel-preset',
+    'nativewind/babel',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
+    'react-native-reanimated/plugin', // 必须放最后
+  ],
 };
